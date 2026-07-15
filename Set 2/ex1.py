0ab1071b581da1d94fe553f1c2f9ab4c7b4d5260
Buiceac Robert <buiceacr@gmail.com>
2026-07-15 18:30:34 +0300

    ex1

---
+++ Set 2/ex1.py
@@ -0,0 +1,11 @@
+def fibonacci(n:int)->list:
+    if n==0:
+        return []
+    elif n==1:
+        return [0]
+    fib=[0,1]
+    for i in range(2,n):
+        fib.append(fib[i-1]+fib[i-2])
+    return fib
+
+print(fibonacci(10))
